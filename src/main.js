ff31f07bc8986dd0250508a583857d1221502a9e
Igorth <igordiasth@gmail.com>
2020-03-23 15:47:23 +1300

    add smoke tests from search

---
+++ src/main.js
@@ -0,0 +1,5 @@
+export const search = () => {};
+export const searchAlbuns = () => {};
+export const searchTracks = () => {};
+export const searchPlaylists = () => {};
+export const searchArtists = () => {};
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,29 @@
 import { expect } from 'chai';
+import { search, searchAlbuns, searchTracks, searchPlaylists, searchArtists } from '../src/main';
 
-describe('Main', () => {
+describe('Spotify Wrapper', () => {
+
+  describe('Smoke tests', () => {
+    // search generico -> + mais de 1 tipo;
+    // search albuns;
+    // search tracks;
+    // search artists;
+    // search playlists;
+    it('should exist the search method', () => {
+      expect(search).to.exist;
+    });
+    it('should exist the searchAlbuns method', () => {
+      expect(searchAlbuns).to.exist;
+    });
+    it('should exist the searchTracks method', () => {
+      expect(searchTracks).to.exist;
+    });
+    it('should exist the searchArtists method', () => {
+      expect(searchArtists).to.exist;
+    });
+    it('should exist the searchPlaylists method', () => {
+      expect(searchPlaylists).to.exist;
+    });
+  });
 
 });
